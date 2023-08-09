513c32df69694a7f1e61925ea6cc375baab429e0
dante4rt <rxmxdhxni@gmail.com>
2023-08-09 21:01:08 +0700

    feat: add weather report for 5 days ahead

---
+++ Ramalan-Cuaca-Jakarta/index.js
@@ -0,0 +1,28 @@
+// https://api.openweathermap.org/data/2.5/forecast?q=Jakarta&appid=b11964ad9151b0dc00a8877b1a7c4fea&units=metric
+require('dotenv').config()
+const moment = require('moment')
+const URL = `https://api.openweathermap.org/data/2.5/forecast?q=Jakarta&units=metric&appid=` + process.env.SECRET
+
+async function getData() {
+    const response = await fetch(URL, {
+        method: 'GET'
+    })
+    const data = await response.json()
+    return data;
+}
+
+function checkData() {
+    getData()
+    .then((obj) => {
+        for (let i = 0; i < obj.list.length; i++) {
+            let perHour = obj.list[i]
+            let date = perHour.dt_txt
+            if (date.includes('00:00:00')) {
+                console.log(moment(date).format('ddd, DD MMM YYYY') + `: ${perHour.main.temp}°C`);
+            }
+        }
+    })
+}
+
+// console.log(checkData(data));
+checkData()
